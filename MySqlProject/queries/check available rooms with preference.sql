2a8c5ced38b07e4cc7f6d0f20495b3a5e4e50163
basamakis <75481484+basamakis@users.noreply.github.com>
2021-01-10 22:01:34 +0200

    Delete check available rooms with preference.sql

--- MySqlProject/queries/check available rooms with preference.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT h.name ,rt.id_roomType,h.stars,rt.capacity,rt.suite,rt.pool,rt.hot_tub,rt.smokers,rt.kitchen,rt.bar
-from hotel_rooms hr
-join reservation r on hr.hr_id_hotel=r.r_id_hotel and hr.hr_id_roomType = r.r_id_roomType
-join hotel h on hr.hr_id_hotel = h.id_hotel
-join room_type rt on rt.id_roomType = hr.hr_id_roomType
-where ('2020-12-30' > r.real_check_out and h.country = 'Pakistan' and h.city = 'Kamālia'  and rt.suite='NO' and rt.pool='NO'
-and rt.hot_tub='YES' and rt.smokers='YES'and rt.kitchen='YES' and rt.bar='YES')
